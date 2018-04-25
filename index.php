7c655fbc17bf5e9f588d217415a352094f2a0e50
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-04-25 10:31:18 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EcoFlavor</title>
+</head>
+<body>
+   <header>
+        <img src="images/home.png" alt="home">
+        <img src="images/instellingen.png" alt="instellingen">
+   </header>
+
+   <div>
+        <input type="search" value="zoek jouw product">
+   </div>
+
+   <div>
+        <h1>Product van de maand</h1>
+        <h2>De Prei</h2>
+        <p>Ben je aan het koken? Voeg dat wat prei toe aan het gerecht… 
+        het zorgt voor een rijkere en zachtere smaak. 
+        Een prei is ook ideaal als basis voor een soep,
+        maar het kan evengoed gestoomd of gebakken worden in boter.</p
+        <a href="maandProduct.php">Lees Meer</a>
+   </div>
+   <main>
+        <h3>Categorieën</h3>
+        <div>
+            <img src="" alt="bio">
+            <p>Bio Producten</p>
+            <div> </div>
+        </div>
+
+        <div>
+            <img src="" alt="streek">
+            <p>Streekproducten</p>
+            <div> </div>
+        </div>
+
+        <div>
+            <img src="" alt="fairtrade">
+            <p>Fairtrade Producten</p>
+            <div> </div>
+        </div>
+
+        <div>
+            <img src="" alt="seizoen">
+            <p>Seizoensproducten</p>
+            <div> </div>
+        </div>
+   </main>
+
+   <div>
+        <h3>Handenvrij Shoppen</h3>
+        <p>Sinds december '15 startten de ECOkoeriers met een unieke dienstverlening naar de shoppers.
+        Laat je aankopen achter in de winkels, de ECOkoeriers pikken ze voor je op en zorgen ervoor dat ze later die dag bij je thuis geleverd worden. </p>
+        <a href="ecokoeriers.php">Lees Meer</a>
+   </div>
+</body>
+</html>
