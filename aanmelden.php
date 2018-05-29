f3e50164af06ce796c128565ef0dcc2b025a875f
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-05-29 14:53:57 +0200

    aanmelden.php

--- aanmelden.php
+++ aanmelden.php
@@ -3,21 +3,26 @@
 
 <body>
 
-<header>
-    <img src="" alt="logo">
-</header>
+<div class="aanmelden__logo">
+    <img src="images/logo_ecoflavor.png" alt="logo" class="ecoflavorlogo">
+</div>
 
-<main>
-    <label for="email">Email</label>
-    <input type="text" name="email">
+<main class="aanmeldingsformulier">
+    <div class="aanmelden__formulier">
+    <label class="aanmelden__label" for="email">Email</label>
+    <input class="aanmelden__input" type="text" name="email">
 
-    <label for="wachtwoord">Wachtwoord</label>
-    <input type="text" name="wachtwoord">
+    <label class="aanmelden__label" for="wachtwoord">Wachtwoord</label>
+    <input class="aanmelden__input" type="text" name="wachtwoord">
 
-    <input type="submit" value="Aanmelden">
+    <a href="index.php" class="btn btn--aanmelden">Aanmelden<span class="border border--aanmelden"></span></a>
 
-    <a href="registreren.php">account aanmaken</a>
-    <a href="#">wachtwoord vergeten</a>
+    <div class="aanmelden__links">
+        <a class="link__account" href="registreren.php">account aanmaken</a>
+        <a class="link__wachtwoord" href="#">wachtwoord vergeten</a>
+    </div>
+
+    </div>
 </main>
     
 </body>
