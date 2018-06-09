a68e5e0dc1f2c0bf8328f07b8a56f4a1ddd542d5
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-06-09 16:35:44 +0200

    bugs fixed

--- nav.inc.php
+++ nav.inc.php
@@ -1,5 +1,5 @@
 <nav class="nav">
-    <li class="nav__item nav__profiel"><a href="profiel.php"><img src="images/profiel.png" alt="profiel">Profiel</a></li>
+    <li class="nav__item nav__profiel"><a href="#"><img src="images/profiel.png" alt="profiel">Profiel</a></li>
     <li class="nav__item"><a href="toevoegen.php"><img src="images/toevoegen.png" alt="plus">Toevoegen</a></li>
     <li class="nav__item nav__mand"><a href="winkelmandje.php"><img src="images/kar.png" alt="mandje">Winkelmandje</a></li>
     <li class="nav__item"><a href="zoek.php"><img src="images/zoeken.png" alt="zoeken">Zoeken</a></li>
